3a1741aa4de04fa52a480d042b7d38465607592e
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2023-01-08 23:57:52 +0300

    -

--- AppcentCase/Network/EndPoint.swift
+++ AppcentCase/Network/EndPoint.swift
@@ -12,3 +12,48 @@ public enum Endpoint: Equatable {
 case topHeadlines(countryCode: String)
 case everything(query: String, languageCode: String)
 }
+
+extension Endpoint {
+    var urlString: String {
+        return "\(NetworkHelper.shared.BASE_URL)/\(version)/\(path)"
+    }
+    var version: String {
+        switch self {
+        case .topHeadlines:
+            return "v2"
+        case .everything:
+            return "v2"
+        }
+    }
+    var path: String {
+        switch self {
+        case .topHeadlines:
+            return "top-headlines"
+        case.everything:
+            return "everything"
+        }
+    }
+}
+    extension Endpoint {
+        var httpMethod: HTTPMethod {
+            switch self {
+            case .topHeadlines,
+                    .everything:
+                return .get
+            }
+        }
+    }
+
+extension Endpoint {
+    var parameters: Parameters? {
+        switch self {
+        case .topHeadlines(let countryCode):
+            return [
+                "apiKey": NetworkHelper.shared.API_KEY,
+                "pageSize": NetworkHelper.shared.API_PAGE_LIMIT
+                "country": countryCode
+                "category": "general"
+            ]
+        }
+    }
+}
